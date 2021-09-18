41efa0801fb2cbb92fed53f34efe26008cb13230
Nitin Singla <nitin.singla@notesroom.com>
2021-09-18 09:36:55 +0530

    Updated jib plugin

--- dockerize.sh
+++ dockerize.sh
@@ -1,1 +1,1 @@
-mvn compile com.google.cloud.tools:jib-maven-plugin:2.8.0:build -Dimage=gcr.io/directed-mender-323809/hello-world
+mvn compile com.google.cloud.tools:jib-maven-plugin:3.1.4:build -Dimage=gcr.io/directed-mender-323809/hello-world
