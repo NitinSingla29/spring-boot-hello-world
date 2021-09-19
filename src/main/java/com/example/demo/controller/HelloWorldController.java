8594eb553dcd0ab67759bd80a74bdb79efd7a9e8
Nitin Singla <nitin.singla@notesroom.com>
2021-09-19 18:56:37 +0530

    Added host name in hello world message

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -5,12 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Controller
 @EnableAutoConfiguration
 public class HelloWorldController {
   @RequestMapping("/")
   @ResponseBody
   public String sayHello() {
-    return "Hello World Version1";
+    return "Hello World Version1 from Host:" + getHostName();
+  }
+
+  private String getHostName()
+  {
+    try
+    {
+      InetAddress addr;
+      addr = InetAddress.getLocalHost();
+      return addr.getHostName();
+    }
+    catch (UnknownHostException ex)
+    {
+      throw new RuntimeException("Hostname can not be resolved");
+    }
   }
 }
